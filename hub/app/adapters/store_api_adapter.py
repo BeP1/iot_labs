dec293b7191c5762d30d6e62f1223fb63c79e7d4
BeP1 <artemneigsal@gmail.com>
2026-03-31 16:30:41 +0200

    lab3: complete implementation of hub module

--- hub/app/adapters/store_api_adapter.py
+++ hub/app/adapters/store_api_adapter.py
@@ -1,8 +1,5 @@
-import json
 import logging
 from typing import List
-
-import pydantic_core
 import requests
 
 from app.entities.processed_agent_data import ProcessedAgentData
@@ -13,12 +10,21 @@ class StoreApiAdapter(StoreGateway):
     def __init__(self, api_base_url):
         self.api_base_url = api_base_url
 
-    def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]):
-        """
-        Save the processed road data to the Store API.
-        Parameters:
-            processed_agent_data_batch (dict): Processed road data to be saved.
-        Returns:
-            bool: True if the data is successfully saved, False otherwise.
-        """
-        # Implement it
+    def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]) -> bool:
+        try:
+            url = f"{self.api_base_url}/processed_agent_data/"
+
+            payload = [item.model_dump(mode="json") for item in processed_agent_data_batch]
+
+            response = requests.post(url, json=payload)
+
+            if response.status_code == 200:
+                logging.info("Batch successfully sent to Store API")
+                return True
+            else:
+                logging.error(f"Failed to send data: {response.text}")
+                return False
+
+        except Exception as e:
+            logging.error(f"Error sending data to Store API: {e}")
+            return False
